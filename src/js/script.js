7fe254ac494ffaee0ba9a24d10e1f43ddd5c50a9
Eduardo Luis Borba <eduardoborba3@gmail.com>
2022-01-20 15:57:56 -0300

    Accordion feito

--- src/js/script.js
+++ src/js/script.js
@@ -23,3 +23,24 @@ function initTabNav(){
 
 initTabNav();
 
+
+function initAccordion(){
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+
+    if(accordionList.length){
+        accordionList[0].classList.add('active');
+        accordionList[0].nextElementSibling.classList.add('active');
+        
+        
+        function activeAccordion(){
+            this.classList.toggle('active');
+            this.nextElementSibling.classList.toggle('active');
+        }
+        
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion);
+        });
+    }
+}
+
+initAccordion();
