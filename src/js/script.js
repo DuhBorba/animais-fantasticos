4e7bde0ed960089e94370ecc3b4ad021d192fde0
Eduardo Luis Borba <eduardoborba3@gmail.com>
2022-01-21 11:54:48 -0300

    Smooth scroll feito

--- src/js/script.js
+++ src/js/script.js
@@ -44,3 +44,35 @@ function initAccordion(){
 }
 
 initAccordion();
+
+
+function initSmoothScroll(){
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSection(event){
+        event.preventDefault();
+        const href = event.target.getAttribute('href');
+        let section = document.querySelector(href);
+
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+
+        // Forma alternativa
+
+        // const topo = section.offsetTop;
+
+        // window.scrollTo({
+        //     top: topo,
+        //     behavior: 'smooth',
+        // });
+
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
+    });
+}
+
+initSmoothScroll();
