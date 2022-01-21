715953922e139998b600458373f0f4266b9348b8
Eduardo Luis Borba <eduardoborba3@gmail.com>
2022-01-21 12:57:37 -0300

    Anima scroll feito

--- src/js/script.js
+++ src/js/script.js
@@ -76,3 +76,30 @@ function initSmoothScroll(){
 }
 
 initSmoothScroll();
+
+
+function initAnimaScroll(){
+    const sections = document.querySelectorAll('.js-scroll');
+
+    if(sections.length){
+        const windowMetade = window.innerHeight * 0.7;
+
+        function animaScroll(){
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+
+                if(sectionTop < windowMetade){
+                    section.classList.add('active');
+                } else {
+                    section.classList.remove('active');
+                }
+            })
+        }
+
+        animaScroll();
+
+        window.addEventListener('scroll', animaScroll);
+    }
+}
+
+initAnimaScroll();
