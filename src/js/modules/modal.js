a8dcaa7429c4211a205c661db71c2c7324026b87
Eduardo Luis Borba <eduardoborba3@gmail.com>
2022-01-23 12:02:21 -0300

    Modal feito

---
+++ src/js/modules/modal.js
@@ -0,0 +1,24 @@
+export default function initModal(){
+  const btnOpen = document.querySelector('[data-modal="open"]');
+  const btnClose = document.querySelector('[data-modal="close"]');
+  const containerModal = document.querySelector('[data-modal="container"]');
+  
+  if(btnOpen && btnClose && containerModal){
+  
+    function toggleModal(event){
+      event.preventDefault();
+      containerModal.classList.toggle('active');
+    }
+  
+    function clickOutModal(event){
+      if(event.target === this){
+        toggleModal(event);
+      }
+    }
+  
+    btnOpen.addEventListener('click', toggleModal);
+    btnClose.addEventListener('click', toggleModal);
+    containerModal.addEventListener('click', clickOutModal);
+  
+  } 
+}
--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,10 @@ import initSmoothScroll from "./modules/smooth-scroll.js";
 import initTabNav from "./modules/tab-nav.js";
 import initAccordion from "./modules/accordion.js";
 import initAnimaScroll from "./modules/anima-scroll.js";
+import initModal from "./modules/modal.js"
 
 initSmoothScroll();
 initTabNav();
 initAccordion();
 initAnimaScroll();
+initModal();
