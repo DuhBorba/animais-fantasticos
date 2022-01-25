78d03ac5b446eef7f9f89148cbb3fa93f4f65afc
Eduardo Luis Borba <eduardoborba3@gmail.com>
2022-01-24 23:27:59 -0300

    Tooltip feito

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,11 @@ import initTabNav from "./modules/tab-nav.js";
 import initAccordion from "./modules/accordion.js";
 import initAnimaScroll from "./modules/anima-scroll.js";
 import initModal from "./modules/modal.js"
+import initTooltip from "./modules/tooltip.js"
 
 initSmoothScroll();
 initTabNav();
 initAccordion();
 initAnimaScroll();
 initModal();
+initTooltip();
