4100f53bbb077e6cb2be6cf5145a03cbcaab3ecc
Eduardo Luis Borba <eduardoborba3@gmail.com>
2022-02-01 22:49:58 -0300

    Anima numbers feito

---
+++ src/js/modules/anima-numbers.js
@@ -0,0 +1,34 @@
+export default function initAnimaNumbers(){
+  function animaNumbers(){
+    const numbers = document.querySelectorAll('[data-numero]');
+    
+    numbers.forEach((number) => {
+      const total = +number.innerText;
+    
+      const incremento = Math.floor(total / 100);
+      let start = 0;
+      
+      const timer = setInterval(() => {
+        start = start + incremento;
+        number.innerText = start;
+        if(start > total){
+          number.innerText = total;
+          clearInterval(timer);
+        }
+      }, 25 * Math.random())
+    })
+  }
+  
+  
+  function handleMutation(mutation){
+    if(mutation[0].target.classList.contains('active')){
+      observer.disconnect();
+      animaNumbers();
+    }
+  }
+  
+  const observerTarget = document.querySelector('.numeros');
+  const observer = new MutationObserver(handleMutation);
+  
+  observer.observe(observerTarget, {attributes: true});
+}
