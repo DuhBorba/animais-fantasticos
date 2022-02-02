01c2418a90a01f31bc6a465be9bc68ad92eb7f6a
Eduardo Luis Borba <eduardoborba3@gmail.com>
2022-02-02 15:10:36 -0300

    Form validate feito

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,13 @@ import initSmoothScroll from "./modules/smooth-scroll.js";
 import initTabNav from "./modules/tab-nav.js";
 import initAccordion from "./modules/accordion.js";
 import initAnimaScroll from "./modules/anima-scroll.js";
-import initModal from "./modules/modal.js"
-import initTooltip from "./modules/tooltip.js"
-import initDropdownMenu from "./modules/dropdown-menu.js"
-import initMenuMobile from "./modules/menu-mobile.js"
-import initAnimaNumbers from "./modules/anima-numbers.js"
-import initFuncionamento from "./modules/funcionamento.js"
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initAnimaNumbers from "./modules/anima-numbers.js";
+import initFuncionamento from "./modules/funcionamento.js";
+import initValidateForm from "./modules/validate-form.js";
 
 initSmoothScroll();
 initTabNav();
@@ -19,3 +20,4 @@ initDropdownMenu();
 initMenuMobile();
 initAnimaNumbers();
 initFuncionamento();
+initValidateForm();
