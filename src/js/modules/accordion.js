ba6715c9f528fb3736cbafb5f58eb46a8399db65
Eduardo Luis Borba <eduardoborba3@gmail.com>
2022-02-10 09:16:31 -0300

    Refatoração accordion para Class

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,18 +1,21 @@
-export default function initAccordion(){
-    const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
-
-    if(accordionList.length){
-        accordionList[0].classList.add('active');
-        accordionList[0].nextElementSibling.classList.add('active');
-        
-        
-        function activeAccordion(){
-            this.classList.toggle('active');
-            this.nextElementSibling.classList.toggle('active');
-        }
-        
-        accordionList.forEach((item) => {
-            item.addEventListener('click', activeAccordion);
+export default class Accordion{
+    constructor(list){
+      this.accordionList = document.querySelectorAll(list);
+      this.activeClass = 'active';
+    }
+    toggleAccordion(item){
+        item.classList.toggle(this.activeClass);
+        item.nextElementSibling.classList.toggle(this.activeClass);
+    }
+    addAccordionEvent(){
+        this.accordionList.forEach((item) => {
+            item.addEventListener('click',() => this.toggleAccordion(item));
         });
+    }
+    init(){
+        if(this.accordionList.length){
+            this.toggleAccordion(this.accordionList[0]);
+            this.addAccordionEvent();
+        }
     }
 }
--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 import SmoothScroll from "./modules/smooth-scroll.js";
+import Accordion from "./modules/accordion.js";
 import initTabNav from "./modules/tab-nav.js";
-import initAccordion from "./modules/accordion.js";
 import initAnimaScroll from "./modules/anima-scroll.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
@@ -16,8 +16,10 @@ import initCountdownSeasons from "./modules/countdown-seasons.js";
 const smoothScroll = new SmoothScroll('[data-menu="smooth"] a[href^="#"]');
 smoothScroll.init();
 
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
 initTabNav();
-initAccordion();
 initAnimaScroll();
 initModal();
 initTooltip();
